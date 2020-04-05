d8afb75d109faf2cdbaccee63a86b09eb1f6afaa
Wojciech Gaudnik <wojciech.gaudnik@gmail.com>
2020-04-05 19:11:14 +0200

    Ready for deploy

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -4,35 +4,84 @@ import {SearchBar} from "../SearchBar/SearchBar";
 import {SearchResults} from "../SearchResults/SearchResults";
 import {PlayList} from "../Playlist/PlayList";
 
+import {Spotify} from "../../util/Spotify";
+
 class App extends React.Component{
     constructor(props) {
         super(props);
 
         this.state = {
             searchResults: [
-                {name: 'name1', artist: 'artist1', album: 'album1', id: 1},
-                {name: 'name2', artist: 'artist2', album: 'album2', id: 2},
-                {name: 'name3', artist: 'artist3', album: 'album3', id: 3}
-                ],
+                // {name: 'name1', artist: 'artist1', album: 'album1', id: 1},
+                // {name: 'name2', artist: 'artist2', album: 'album2', id: 2},
+                // {name: 'name3', artist: 'artist3', album: 'album3', id: 3}
+            ],
             playListName: 'My Play List',
             playListTracks: [
-                {name: 'playListName1', artist: 'playListArtist1', album: 'playListAlbum1', id: 4},
-                {name: 'playListName2', artist: 'playListArtist2', album: 'playListAlbum2', id: 5},
-                {name: 'playListName3', artist: 'playListArtist3', album: 'playListAlbum3', id: 6},
-                ]
-        }
+                // {name: 'playListName1', artist: 'playListArtist1', album: 'playListAlbum1', id: 4},
+                // {name: 'playListName2', artist: 'playListArtist2', album: 'playListAlbum2', id: 5},
+                // {name: 'playListName3', artist: 'playListArtist3', album: 'playListAlbum3', id: 6},
+            ]
+        };
+        this.addTrack = this.addTrack.bind(this);
+        this.removeTrack = this.removeTrack.bind(this);
+        this.updatePlayListName = this.updatePlayListName.bind(this);
+        this.savePlayList = this.savePlayList.bind(this);
+        this.search = this.search.bind(this);
     }
+
+    addTrack(track){
+        let tracks = this.state.playListTracks;
+        if (tracks.find(savedTrack => savedTrack.id === track.id)) {
+            return;
+        }
+        tracks.push(track);
+        this.setState({playListTracks: tracks})
+    }
+
+    removeTrack(track){
+        let tracks = this.state.playListTracks;
+        tracks = tracks.filter(currentTrack => currentTrack.id !== track.id);
+        this.setState({playListTracks: tracks});
+
+    }
+
+    updatePlayListName(name){
+        console.log(name.value + " <<");
+        this.setState({playListName: name})
+    }
+
+    savePlayList(){
+        const trackUris = this.state.playListTracks.map(track => track.uri);
+        Spotify.savePlayList(this.state.playListName, trackUris).then(() => {this.setState({
+                playListName: 'New PlayList',
+                playListTracks: []
+            })
+        })
+    }
+
+    search(term){
+        console.log("aftert click");
+        Spotify.search(term).then(searchResults => {
+            this.setState({searchResults: searchResults})
+        })
+    }
+
     render() {
         return(
             <div>
                 <h1>Ja<span className="highlight">mmm</span>ing</h1>
                 <div className="App">
-                    <SearchBar />
+                    <SearchBar onSearch={this.search}/>
                     <div className="App-playlist">
-                        <SearchResults searchResults={this.state.searchResults}/>
+                        <SearchResults searchResults={this.state.searchResults}
+                        onAdd={this.addTrack}/>
                         <PlayList
                             playListName={this.state.playListName}
-                            playListTracks={this.state.playListTracks}/>
+                            playListTracks={this.state.playListTracks}
+                            onRemove={this.removeTrack}
+                            onNameChange={this.updatePlayListName}
+                            onSave={this.savePlayList}/>
                     </div>
                 </div>
             </div>
--- src/Components/SearchBar/SearchBar.js
+++ src/Components/SearchBar/SearchBar.js
@@ -2,11 +2,30 @@ import React from 'react';
 import './SearchBar.css';
 
 export class SearchBar extends React.Component{
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            term: ''
+        };
+
+        this.search = this.search.bind(this);
+        this.handleTermChange = this.handleTermChange.bind(this);
+    }
+
+    search(){
+        this.props.onSearch(this.state.term);
+    }
+
+    handleTermChange(event) {
+        console.log("SearchBar  test");
+        this.setState({term: event.target.value})
+    }
     render() {
         return(
             <div className="SearchBar">
-                <input placeholder="Enter A Song, Album, or Artist"/>
-                <button className="SearchButton">SEARCH</button>
+                <input onChange={this.handleTermChange} placeholder="Enter A Song, Album, or Artist"/>
+                <button onClick={this.search} className="SearchButton">SEARCH</button>
             </div>
         )
     }
